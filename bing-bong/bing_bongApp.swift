8139b172c0887be0cdfbd379626643760a28e307
rob kerr <reallyrobkerr@gmail.com>
2024-01-10 01:21:13 -0800

    settings, whitespace

--- bing-bong/bing_bongApp.swift
+++ bing-bong/bing_bongApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct bing_bongApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
