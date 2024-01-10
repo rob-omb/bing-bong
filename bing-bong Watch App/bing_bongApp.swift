e361374bc9d2c9c6875a6dcc5d01d2b1a2f60e97
rob kerr <reallyrobkerr@gmail.com>
2024-01-10 00:09:11 -0800

    Initial Commit

---
+++ bing-bong Watch App/bing_bongApp.swift
@@ -0,0 +1,17 @@
+//
+//  bing_bongApp.swift
+//  bing-bong Watch App
+//
+//  Created by rob kerr on 2024-01-10.
+//
+
+import SwiftUI
+
+@main
+struct bing_bong_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bing-bong/bing_bongApp.swift
@@ -0,0 +1,17 @@
+//
+//  bing_bongApp.swift
+//  bing-bong
+//
+//  Created by rob kerr on 2024-01-10.
+//
+
+import SwiftUI
+
+@main
+struct bing_bongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
