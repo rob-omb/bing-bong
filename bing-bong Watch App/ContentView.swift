8163795a5699763fab50c3cda592281a79f57c72
rob kerr <reallyrobkerr@gmail.com>
2024-01-19 22:28:55 -0800

    Establish layout and basic UI with mock BPM

--- bing-bong Watch App/ContentView.swift
+++ bing-bong Watch App/ContentView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State var bpm = 120;
+  @State var isPlaying = false
+  @State var buttonIcon = Image(systemName: "play.fill")
+
+  var body: some View {
+    VStack(spacing: 5) {
+      Text("BPM: \(bpm)")
+        .font(.title)
+      Button(
+        action: { togglePlayPause() },
+        label: { buttonIcon }
+      )
+        .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+        .clipShape(Circle())
+        .foregroundColor(Color.accentColor)
     }
+    .foregroundColor(.accentColor)
+    Spacer()
+  }
+
+  func togglePlayPause() {
+    if isPlaying {
+      isPlaying = false
+      buttonIcon = Image(systemName: "play.fill")
+    } else {
+      isPlaying = true
+      buttonIcon = Image(systemName: "stop.fill")
+    }
+  }
 }
 
+// macro for what to render in the canvas
 #Preview {
-    ContentView()
+  ContentView()
 }
